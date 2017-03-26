dadd888f3a2cb2f79738791b900aafe738f1301b
Daniel Platek <danny.platek@gmail.com>
2017-03-26 03:02:11 +0200

    touchmove-off

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(function() {
 
     //prevent native touch activity like scrolling
-    $('html, body').on('touchstart touchmove', function (e) {
+    $('html, body').on('touchmove', function (e) {
         e.preventDefault();
     });
 });
